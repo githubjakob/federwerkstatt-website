56f7764956bb56d6d155ed242c00540fff43ab60
Jakob Zanker <jakob@project-lary.com>
2023-02-22 17:15:39 +0100

    save

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/federwerkstatt-website',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
 }
 
 module.exports = nextConfig
